7f4808561f91dfd1c5ce083504b4526f9a0e4b15
KYLEXY78 <phillipabu31@gmail.com>
2025-05-09 07:14:19 +0100

    changes to the config.yml file

---
+++ frontend/build/precache-manifest.ff992baec309fcf6b30cd35c1412f66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c8e25d308bb49070261331ca9521c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d52927938f717a52a0d",
+    "url": "/static/js/2.02ebc412.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/static/js/2.02ebc412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5df965333b4a7c310b6",
+    "url": "/static/js/main.fa8aef34.chunk.js"
+  },
+  {
+    "revision": "e17a852da7cb9fa7c318",
+    "url": "/static/js/runtime-main.fa5413ac.js"
+  }
+]);
