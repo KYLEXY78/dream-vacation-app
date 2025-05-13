dc876f925e7bbd55df34d725a1cfac6084a1e510
KYLEXY78 <phillipabu31@gmail.com>
2025-05-13 08:32:14 +0100

    port listen

--- backend/server.js
+++ backend/server.js
@@ -102,6 +102,6 @@ app.get('/api/healthcheck', (req, res) => {
 });
 
 // Start Server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
